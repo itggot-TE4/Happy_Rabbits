8ee90fba431b052f7c0581e82320354cd1b8e153
Henrik Stahl <henrik.stahl@elev.ga.ntig.se>
2020-09-18 13:43:27 +0200

    removed the final fruits :skull::green_apple:

--- lib/pluggy/controllers/index_controller.ex
+++ lib/pluggy/controllers/index_controller.ex
@@ -1,7 +1,6 @@
 defmodule Pluggy.IndexController do
   require IEx
 
-  alias Pluggy.Fruit
   alias Pluggy.User
   import Pluggy.Template, only: [render: 2, srender: 2]
   import Plug.Conn, only: [send_resp: 3]
@@ -26,35 +25,27 @@ defmodule Pluggy.IndexController do
   # def show(conn, id), do: send_resp(conn, 200, srender("views/show", fruit: Fruit.get(id)))
   # def edit(conn, id), do: send_resp(conn, 200, srender("views/edit", fruit: Fruit.get(id)))
 
-  def create(conn, params) do
-    Fruit.create(params)
-    case params["file"] do
-      nil -> IO.puts("No file uploaded")  #do nothing
-        # move uploaded file from tmp-folder
-      _  -> File.rename(params["file"].path, "priv/static/uploads/#{params["file"].filename}")
-    end
-    redirect(conn, "/fruits")
-  end
+  # def create(conn, params) do
+  #   Fruit.create(params)
+  #   case params["file"] do
+  #     nil -> IO.puts("No file uploaded")  #do nothing
+  #       # move uploaded file from tmp-folder
+  #     _  -> File.rename(params["file"].path, "priv/static/uploads/#{params["file"].filename}")
+  #   end
+  #   redirect(conn, "/fruits")
+  # end
 
-  def update(conn, id, params) do
-    Fruit.update(id, params)
-    redirect(conn, "/fruits")
-  end
+  # def update(conn, id, params) do
+  #   Fruit.update(id, params)
+  #   redirect(conn, "/fruits")
+  # end
 
-  def destroy(conn, id) do
-    Fruit.delete(id)
-    redirect(conn, "/fruits")
-  end
+  # def destroy(conn, id) do
+  #   Fruit.delete(id)
+  #   redirect(conn, "/fruits")
+  # end
 
   defp redirect(conn, url) do
     Plug.Conn.put_resp_header(conn, "location", url) |> send_resp(303, "")
-  end
-
-  def schoolrenderer([]) do end
-
-  def schoolrenderer([head|tail] = yeah) do
-    schoolrenderer(tail)
-    IO.inspect(head)
-    p="1"
   end
 end
