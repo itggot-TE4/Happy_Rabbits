dd14a7076c4c3bdb71bcc895aa1483811c34b76a
Henrik Stahl <henrik.stahl@elev.ga.ntig.se>
2020-09-22 10:03:19 +0200

    resolved #5 :hammer::cn:

--- lib/pluggy/models/quiz.ex
+++ lib/pluggy/models/quiz.ex
@@ -1,18 +1,20 @@
 defmodule Pluggy.Quiz do
     defstruct(id: nil, group_name: "")
 
+    alias Pluggy.Quiz
+
     def get_students(group) do
         group_id = Postgrex.query!(DB, "SELECT id FROM groups WHERE name = $1", [group], pool: DBConnection.ConnectionPool).rows
         [head | _tail] = group_id
         [head | _tail] = head
         Postgrex.query!(DB, "SELECT id, name FROM students WHERE group_id = $1", [head], pool: DBConnection.ConnectionPool).rows
         |> IO.inspect
-        |> Pluggy.Quiz.to_struct_list
+        |> to_struct_list
         # |> IO.inspect
     end
 
     def to_struct([[id, name]]) do
-        %Quiz{id: id, name: name}
+        %Quiz{id: id, group_name: name}
     end
 
     # fungerar inte hahahahahhaahahahahahhahac hjagfg äslakrt elsiuxjkdawera
